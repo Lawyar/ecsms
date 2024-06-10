37792a9a50d96e66c6caa7fc07fcea25526dec56
Denis Melnik <>
2024-06-10 13:26:20 +0300

    new changes in analytics and new window

--- libraries/analytics/inc/analytics.h
+++ libraries/analytics/inc/analytics.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#ifndef ANALYTICS_H
+#define ANALYTICS_H
+
+#include <vector>
+
+class Analytics {
+ public:
+  // Функция для сравнения данных
+  template <typename T>
+  void compareData(const std::vector<T>& data1, const std::vector<T>& data2);
+
+  // Функция для конвертации данных в определенный формат
+  template <typename T>
+  void convertData(const std::vector<T>& data);
+
+  // Функция для группировки информации по определенным признакам
+  template <typename T>
+  void groupData(const std::vector<T>& data, int groupSize);
+
+  // Другие функции анализа данных
+};
+
+#endif  // ANALYTICS_H
