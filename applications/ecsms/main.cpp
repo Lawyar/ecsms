66550557ff544bf05304285469cea4e406088474
wellcome2AD <iuuyyttttt@mail.ru>
2024-06-15 13:59:04 +0300

    change font size

--- applications/ecsms/main.cpp
+++ applications/ecsms/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QFont font = a.font();
-    font.setPointSize(14);
+    font.setPointSize(11);
     a.setFont(font);
     MainWindow w;
     w.show();
